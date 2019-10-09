0109c4663a0d651e5f95f11045be9f9ac7a41904
homobulla <homo_bulla@outlook.com>
2019-10-09 12:44:02 +0800

    doc node事件发射器和子任务

--- node-interview-questions/EventEmitter.js
+++ node-interview-questions/EventEmitter.js
@@ -1,10 +1,5 @@
-const util = require("util");
-const EventEmitter = require("events").EventEmitter;
-
-function MyEmitter() {
-    EventEmitter.call(this); //构造函数
-}
-util.inherits(MyEmitter, EventEmitter); // 继承
+const EventEmitter = require("events");
+class MyEmitter extends EventEmitter {}
 
 let em = new MyEmitter();
 
@@ -13,3 +8,15 @@ em.on("test", function(data) {
 });
 
 em.emit("test", "传递消息很快");
+var emitter3 = new MyEmitter();
+// 当任何on事件添加到EventEmitter时，就会触发newListener事件
+emitter3.on("newListener", function(name, listener) {
+    console.log("新事件的名字:", name);
+    console.log("新事件的代码:", listener);
+    setTimeout(function() {
+        console.log("我是自定义延时处理机制");
+    }, 1000);
+});
+emitter3.on("hello", function() {
+    console.log("hello　node");
+});
